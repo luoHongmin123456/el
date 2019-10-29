74df0424f856ecd22c6c7ef9f0cd9c5c02b03173
luo123 <1932811178@qq.com>
2019-10-29 21:31:15 +0800

    first commit

---
+++ src/components/UserAdd.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <el-dialog title="添加用户" :visible.sync="showAdd.show">
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="showAdd.show=false">取 消</el-button>
+                <el-button type="primary" @click="showAdd.show=false">添加</el-button>
+            </div>
+        </el-dialog>
+    </div>
+</template>
+<script>
+export default {
+    props:['showAdd']
+}
+</script>
